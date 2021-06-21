22595c44ec6babef2012f35b269dc593eb1dd34e
Rafal Porzucek <rafal.porzucek02@dixonscarphone.com>
2021-06-21 12:31:22 +0100

    Add consts

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const DOTS = '...';
+
+export const PERSONAL_DETAILS = Object.freeze({
+  id: "ID",
+  firstName: "FIRST NAME",
+  lastName: "LAST NAME",
+  email: "EMAIL",
+  phone: "PHONE"
+})
